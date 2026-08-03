3d11e35ed14e8099e797f88b5c19e10388ab9c7c
Sudarshan <suprabhu2006@gmail.com>
2026-08-03 17:13:42 -0500

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,28 @@
+#Dictionary
+employee={
+    "name": "Alice",
+    "employee_id" : 101,
+    "department":"Operations",
+    "salary":65000.0,
+    "is_manage":True
+
+}
+#Printing each value in the dictionary
+print(employee["name"])
+print(employee["employee_id"])
+print(employee["department"])
+print(employee["salary"])
+print(employee["is_manage"])
+employee["years_experience"]=5
+print(employee["years_experience"])
+#List of 5 products
+products_list=["Phone","Ipad","Laptop","PC","Smart Watch"]
+#First Product of List
+print(products_list[0])
+#Last Product of List
+print(products_list[-1])
+#Tuple of coordinates
+coordinates = (10,20,30)
+print(coordinates[0])
+print(coordinates[1])
+print(coordinates[2])
